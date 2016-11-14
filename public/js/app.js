a36f2384d40fd9a15ecefb0e5daf5f07b1e4ab75
Ubaidillah Waseem <uwaseem@users.noreply.github.com>
2016-11-14 22:53:15 +0800

    Halfway working

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,11 @@ angular.module('onlineAdsApp', ['ui.router'])
   .controller('MainController', function($scope, $http, $state) {
     $scope.users = getAllUsers()
     $scope.products = getAllProducts()
+    $scope.quantity
+    $scope.totalClassicPrice
+    $scope.totalStandoutPrice
+    $scope.totalPremiumPrice
+    $scope.totalPrice
 
     function getAllUsers() {
       console.log('I\'m getting all Users')
@@ -23,18 +28,33 @@ angular.module('onlineAdsApp', ['ui.router'])
         sku: 'classic',
         name: 'Classic Ad',
         price : 269.99,
-        description: 'Basic level of advertisement'
+        description: 'Basic level of advertisement',
+        calcFunction: 'classicCalculate()'
       }, {
         sku: 'standout',
         name: 'Standout Ad',
         price: 322.99,
-        description: 'Able to use company logo and longer presentation texts'
+        description: 'Able to use company logo and longer presentation texts',
+        calcFunction: 'standoutCalculate()'
       }, {
         sku: 'premium',
         name: 'Premium Ad',
         price: 394.99,
-        description: 'Higher visibility plus benefits as Standout Ad'
+        description: 'Higher visibility plus benefits as Standout Ad',
+        calcFunction: 'premiumCalculate()'
       }]
     }
 
+    $scope.classicCalculate = () => {
+      console.log($scope)
+      console.log('I\'m calling the calculate function from classic', $scope.quantity)
+    }
+
+    $scope.standoutCalculate = () => {
+      console.log('I\'m calling the calculate function from standout')
+    }
+
+    $scope.premiumCalculate = () => {
+      console.log('I\'m calling the calculate function from premium')
+    }
   })
