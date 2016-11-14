e21739a8773352cf3552dc6dd7d6c6c133cd8b52
Ubaidillah Waseem <uwaseem@users.noreply.github.com>
2016-11-14 21:38:44 +0800

    get and display users & product

--- public/js/app.js
+++ public/js/app.js
@@ -9,5 +9,32 @@ angular.module('onlineAdsApp', ['ui.router'])
     $urlRouterProvider.otherwise('checkout')
   })
   .controller('MainController', function($scope, $http, $state) {
-    console.log('I\'m calling this MOFO!!')
+    $scope.users = getAllUsers()
+    $scope.products = getAllProducts()
+
+    function getAllUsers() {
+      console.log('I\'m getting all Users')
+      return ['Apple', 'Ford', 'Nike', 'Unilever', 'Others']
+    }
+
+    function getAllProducts() {
+      console.log('I\'m getting all products')
+      return [{
+        sku: 'classic',
+        name: 'Classic Ad',
+        price : 269.99,
+        description: 'Basic level of advertisement'
+      }, {
+        sku: 'standout',
+        name: 'Standout Ad',
+        price: 322.99,
+        description: 'Able to use company logo and longer presentation texts'
+      }, {
+        sku: 'premium',
+        name: 'Premium Ad',
+        price: 394.99,
+        description: 'Higher visibility plus benefits as Standout Ad'
+      }]
+    }
+
   })
