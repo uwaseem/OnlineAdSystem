7e18a1d2bd6bd2fd768f8656fb4eba41090ea916
Ubaidillah Waseem <uwaseem@users.noreply.github.com>
2016-11-17 12:15:54 +0800

    Refactored so breakdown is in one object

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ angular.module('onlineAdsApp', ['ui.router'])
         url: '/orders',
         templateUrl: 'html/orders.html',
         controller: 'MainController'
-      });
+      })
     $urlRouterProvider.otherwise('orders')
   })
   .controller('MainController', function($scope, $http, $state) {
@@ -18,30 +18,11 @@ angular.module('onlineAdsApp', ['ui.router'])
     $scope.products = getAllProducts()
     $scope.userInfo
     $scope.orders = {}
-    $scope.price = {}
     $scope.totalPrice
 
     function getAllUsers() {
       console.log('I\'m getting all Users')
       return ['Please choose your username', 'apple', 'ford', 'nike', 'unilever', 'others']
-    }
-
-    $scope.getUserInfo = () => {
-      // TEMP HACK
-      if ($scope.selectedUser === 'Please choose your username') {
-        $scope.userInfo = null
-        return
-      }
-
-      $http.get('http://localhost:3000/userInfo/' + $scope.selectedUser.toLowerCase())
-        .error((err) => {
-          console.log('why is it failing here - ', err)
-        })
-        .success((data) => {
-          console.log('this is the data received', data)
-          $scope.userInfo = data.userInfo
-          $scope.calculate($scope.selectedUser)
-        })
     }
 
     function getAllProducts() {
@@ -66,6 +47,23 @@ angular.module('onlineAdsApp', ['ui.router'])
       }]
     }
 
+    $scope.getUserInfo = () => {
+      // TEMP HACK UNTIL WE CAN FIGURE OUT DISABLE
+      if ($scope.selectedUser === 'Please choose your username') {
+        $scope.userInfo = null
+        return
+      }
+
+      $http.get('http://localhost:3000/userInfo/' + $scope.selectedUser.toLowerCase())
+        .error((err) => {
+          console.log('why is it failing here - ', err)
+        })
+        .success((data) => {
+          console.log('this is the data received', data)
+          $scope.userInfo = data.userInfo
+        })
+    }
+
     $scope.calculate = function(product) {
       // Get element id
       let productId
@@ -81,23 +79,33 @@ angular.module('onlineAdsApp', ['ui.router'])
 
       // Get discounted price
       // If we meet minimum, set discount
-      if ($scope.userInfo && $scope.userInfo.discountPromo && $scope.userInfo.discountPromo[product] && $scope.orders[product] >= $scope.userInfo.discountPromo[product].minimumOrder) {
+      if ($scope.userInfo && $scope.userInfo.discountPromo && $scope.userInfo.discountPromo[product] && $scope.orders[product].quantity >= $scope.userInfo.discountPromo[product].minimumOrder) {
         pricePerAd = $scope.userInfo.discountPromo[product].newPrice
       }
 
-      $scope.price[product] = (pricePerAd * $scope.orders[product]).toFixed(2)
+      $scope.orders[product].price = (pricePerAd * $scope.orders[product].quantity).toFixed(2)
 
-      console.log($scope.price)
+      console.log($scope.orders)
     }
 
     $scope.determineFreeAds = (product) => {
       // Do we get free stuff
-      if ($scope.userInfo && $scope.userInfo.freePromo && $scope.userInfo.freePromo[product]) {
+      if ($scope.userInfo && $scope.userInfo.freePromo && $scope.userInfo.freePromo[product] && $scope.orders[product]) {
 
         // We need to do the logic here. But what, hmmm ...
-        return Math.floor($scope.orders[product]/$scope.userInfo.freePromo[product].minimumOrder)
+        return Math.floor($scope.orders[product].quantity/$scope.userInfo.freePromo[product].minimumOrder)
       }
 
       return 0
+    }
+
+    $scope.checkout = () => {
+      console.log('this is ordes', $scope.orders)
+      let products = Object.keys($scope.price)
+
+      for(let i in products) {
+
+      }
+
     }
   })
